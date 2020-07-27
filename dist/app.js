bcf5896dbe8064efaa430a7dd3275477e35a002a
lionhard83 <carloleonardi83@gmail.com>
2020-07-27 17:34:14 +0200

    fork in cannon

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const id = "HZ000004";
+let ssid, psw;
 const app = express_1.default();
-app.get('/', (req, res) => {
+app.get('homezero/config', (req, res) => {
     res.json('Hello world');
 });
-app.listen(5000, () => { console.log('Server runngin'); });
+app.post('homezero/config', (req, res) => {
+    if (req.query.ssid && req.query.psw) {
+        ssid = req.query.ssid;
+        psw = req.query.psw;
+    }
+    res.json({
+        id,
+        ssid,
+        psw,
+    });
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => { console.log('Server running'); });
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express';
-
+const id = "HZ000004";
+let ssid: string = '', pwd: string = '';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json('Hello world');
-})
+app.post('/homezero/config', (req, res) => {
+    console.log('req.query:', req.query);
+    if (req.query.ssid && req.query.pwd) {
+        ssid = req.query.ssid;
+        pwd = req.query.pwd;
+    }
+    res.json({ id, ssid, pwd });
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {console.log('Server runngin')});
+app.listen(port, () => { console.log('Server running') });
